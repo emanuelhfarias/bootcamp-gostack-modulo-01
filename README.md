fecf025bde4c60f434eadf24b0a4ec990c825544
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-01-28 23:00:03 -0300

    add Users CRUD

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bootcamp-gostack-modulo-01
 [Bootcamp GoStack#10] modulo 01
+
+```sh
+git clone ...
+cd bootcamp-gostack-modulo-01
+yarn
+yarn dev
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+ *  CRUD
+ *  Resource: Users
+ * 
+ *  Types of Params:
+ *    Route params = /users/1
+ *    Query params = /users?index=1
+ *    Body request = {name: 'emanuel'}
+ */
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const users = ['Emanuel', 'Fernanda'];
+
+// List Users
+server.get('/users', (_, res) => {
+  res.json(users);
+});
+
+// Get User
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+  res.json(users[index]);
+}); 
+
+// Create User
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  res.status(201).json(users);
+});
+
+// Edit User
+server.put('/users', (req, res) => {
+  const { index } = req.query;
+  const { name } = req.body;
+  users[index] = name;
+  res.json(users);
+});
+
+// Delete User
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  res.send();
+});
+
+server.listen(3000);
